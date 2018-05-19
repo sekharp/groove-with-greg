2013b7f9c90a289d000cccc8f944ab2256071c39
Sekhar Paladugu <sekharp@gmail.com>
2018-05-19 17:00:01 -0600

    delete old migration files, have home.js file render records with info

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,83 +1,98 @@
-import React, { Component } from 'react'
-import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
+import React, { Component } from "react";
+import {
+  Container,
+  Header,
+  Segment,
+  Button,
+  Icon,
+  Dimmer,
+  Loader,
+  Divider
+} from "semantic-ui-react";
 
 class Home extends Component {
-  constructor () {
-    super()
-    this.state = {}
-    this.getDrinks = this.getDrinks.bind(this)
-    this.getDrink = this.getDrink.bind(this)
+  constructor() {
+    super();
+    this.state = {};
+    this.getRecords = this.getRecords.bind(this);
+    this.getRecord = this.getRecord.bind(this);
   }
 
-  componentDidMount () {
-    this.getDrinks()
+  componentDidMount() {
+    this.getRecords();
   }
 
-  fetch (endpoint) {
-    return window.fetch(endpoint)
+  fetch(endpoint) {
+    return window
+      .fetch(endpoint)
       .then(response => response.json())
-      .catch(error => console.log(error))
+      .catch(error => console.log(error));
   }
 
-  getDrinks () {
-    this.fetch('/api/drinks')
-      .then(drinks => {
-        if (drinks.length) {
-          this.setState({drinks: drinks})
-          this.getDrink(drinks[0].id)
-        } else {
-          this.setState({drinks: []})
-        }
-      })
+  getRecords() {
+    this.fetch("/api/records").then(records => {
+      if (records.length) {
+        this.setState({ records: records });
+        this.getRecord(records[0].id);
+      } else {
+        this.setState({ records: [] });
+      }
+    });
   }
 
-  getDrink (id) {
-    this.fetch(`/api/drinks/${id}`)
-      .then(drink => this.setState({drink: drink}))
+  getRecord(id) {
+    this.fetch(`/api/records/${id}`).then(record =>
+      this.setState({ record: record })
+    );
   }
 
-  render () {
-    let {drinks, drink} = this.state
-    return drinks
-      ? <Container text>
-        <Header as='h2' icon textAlign='center' color='teal'>
-          <Icon name='unordered list' circular />
-          <Header.Content>
-            List of Ingredients
-          </Header.Content>
+  render() {
+    let { records, record } = this.state;
+    return records ? (
+      <Container text>
+        <Header as="h2" icon textAlign="center" color="teal">
+          <Icon name="unordered list" circular />
+          <Header.Content>Records</Header.Content>
         </Header>
         <Divider hidden section />
-        {drinks && drinks.length
-          ? <Button.Group color='teal' fluid widths={drinks.length}>
-            {Object.keys(drinks).map((key) => {
-              return <Button active={drink && drink.id === drinks[key].id} fluid key={key} onClick={() => this.getDrink(drinks[key].id)}>
-                {drinks[key].title}
-              </Button>
-            })}
-          </Button.Group>
-          : <Container textAlign='center'>No drinks found.</Container>
-        }
+        {records && records.length ? (
+          Object.keys(records).map(key => {
+            return (
+              <div>
+                <Button
+                  active={record && record.id === records[key].id}
+                  fluid
+                  key={key}
+                  onClick={() => this.getRecord(records[key].id)}
+                  color="red"
+                >
+                  {records[key].title}
+                </Button>
+                <br />
+              </div>
+            );
+          })
+        ) : (
+          <Container textAlign="center">No records found.</Container>
+        )}
         <Divider section />
-        {drink &&
+        {record && (
           <Container>
-            <Header as='h2'>{drink.title}</Header>
-            {drink.description && <p>{drink.description}</p>}
-            {drink.ingredients &&
-              <Segment.Group>
-                {drink.ingredients.map((ingredient, i) => <Segment key={i}>{ingredient.description}</Segment>)}
-              </Segment.Group>
-            }
-            {drink.steps && <p>{drink.steps}</p>}
-            {drink.source && <Button basic size='tiny' color='teal' href={drink.source}>Source</Button>}
+            <Header as="h2">Record: {record.title}</Header>
+            {record.artist && <p>Album: {record.artist}</p>}
+            {record.year && <p>Year: {record.year}</p>}
+            {record.condition && <p>Condition: {record.condition}</p>}
           </Container>
-        }
+        )}
       </Container>
-      : <Container text>
+    ) : (
+      <Container text>
         <Dimmer active inverted>
-          <Loader content='Loading' />
+          <Loader content="Loading" />
         </Dimmer>
       </Container>
+    );
   }
 }
 
-export default Home
+export default Home;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 10.times do |r|
   Record.create(
     artist: Faker::Hipster.word,
-    title: Faker::Hipster.sentence.truncate(50),
+    title: Faker::Hipster.sentence.truncate(30),
     year: rand(1958..2018),
     condition: 1
   )
