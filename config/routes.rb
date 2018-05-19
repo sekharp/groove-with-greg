40fa7adb9815067a6f0fc6cc2177f89b86bd80da
Sekhar Paladugu <sekharp@gmail.com>
2018-05-19 15:49:03 -0600

    add route for resource records

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   scope '/api' do
     resources :drinks
+    resources :records
   end
 
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
