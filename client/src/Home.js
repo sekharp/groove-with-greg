12945c1698777d928e2bb7d10159d4aaf7912861
Sekhar Paladugu <sekharp@gmail.com>
2018-05-19 18:58:24 -0600

    build form scaffolding with semantic UI

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,7 +7,9 @@ import {
   Icon,
   Dimmer,
   Loader,
-  Divider
+  Divider,
+  Form,
+  Select
 } from "semantic-ui-react";
 
 class Home extends Component {
@@ -48,13 +50,43 @@ class Home extends Component {
 
   render() {
     let { records, record } = this.state;
+    const options = [
+      { key: "m", text: "Mint", value: "mint" },
+      { key: "g", text: "Good", value: "good" },
+      { key: "a", text: "Acceptable", value: "acceptable" },
+      { key: "b", text: "Bad", value: "bad" }
+    ];
     return records ? (
       <Container text>
         <Header as="h2" icon textAlign="center" color="teal">
-          <Icon name="unordered list" circular />
+          <Icon name="sound" circular />
           <Header.Content>Records</Header.Content>
         </Header>
         <Divider hidden section />
+        <Form>
+          <h3>Add New Record to Collection</h3>
+          <Form.Field>
+            <label>Record Title</label>
+            <input placeholder="Actually Seitan Park Listicle" />
+          </Form.Field>
+          <Form.Field>
+            <label>Album</label>
+            <input placeholder="Gentrify" />
+          </Form.Field>
+          <Form.Field>
+            <label>Year</label>
+            <input placeholder="1977" />
+          </Form.Field>
+          <Form.Field
+            control={Select}
+            label="Condition"
+            options={options}
+            placeholder="Mint"
+          />
+          <Button type="submit">Submit</Button>
+        </Form>
+        <Divider hidden section />
+        <h3>Record Collection</h3>
         {records && records.length ? (
           Object.keys(records).map(key => {
             return (
